1f8dafd16e28ccf82ed5df27bb241664725e95db
cxinval <66485848+cxinval@users.noreply.github.com>
2021-06-25 15:20:28 +0300

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,10 @@
+$('.slider').slick({
+    vertical: true,
+    verticalSwiping: true,
+    infinite: true,
+    autoplaySpeed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    nextArrow: $('.up'),
+    prevArrow: $('.down'),
+  });
